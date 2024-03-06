56974ba15b766dc868e7d712af872d049b8c9704
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-03-06 00:43:34 -0500

    update styles in the CardStyle.css to viewOrder.tsx and viewPayment.tsx

--- src/functions/payment/viewPayment.tsx
+++ src/functions/payment/viewPayment.tsx
@@ -1,6 +1,7 @@
 import {Payment} from "../../classes/Payment.ts";
 import "../../CSS/CardStyle.css";
 import React from "react";
+import {FaEdit, FaTrash} from "react-icons/fa";
 
 interface PaymentCardProps {
     payment: Payment;
@@ -12,21 +13,26 @@ const PaymentCard: React.FC<PaymentCardProps> = ({payment, onEdit, onDelete}) =>
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">{payment.customer}</h5>
-                <p className="card-text">Order ID: {payment.order_id}</p>
-                <p className="card-text">Amount: {payment.amount}</p>
-                <p className="card-text">Payment Date: {payment.payment_date.toString()}</p>
+                <h5 className="card-title text-center">{payment.customer}</h5>
+                <p className="card-text fw-bold">N° orden: <span className={"ms-2"} style={{color: "blueviolet"}}>
+                    <b>{payment.order_id}</b></span></p>
+                <p className="card-text fw-bold">Pagó:<span className={"ms-2"} style={{color: "blueviolet"}}>
+                    S/. {payment.amount}</span></p>
+                <p className="card-text fw-bold">Fecha de Pago:<span className={"ms-2"} style={{color: "blueviolet"}}>
+                        {payment.payment_date.toString()}</span>
+                </p>
                 <div className="d-flex justify-content-end align-items-center mt-3">
                     <button className="btn transparent-btn me-2" onClick={() => onEdit(payment)}>
-                        Editar
+                        <FaEdit/> Editar
                     </button>
                     <button className="btn transparent-btn" onClick={() => onDelete(payment.id)}>
-                        Eliminar
+                        <FaTrash/> Eliminar
                     </button>
                 </div>
             </div>
         </div>
-    );
+    )
+        ;
 }
 
 interface ViewPaymentProps {
@@ -52,34 +58,5 @@ const viewPayment: React.FC<ViewPaymentProps> = ({payments, onEdit, onDelete}) =
         </div>
     );
 }
-
-/*
-const PaymentCard = ({payment}: { payment: Payment }) => {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">Payment ID: {payment.id}</h5>
-                <p className="card-text">Order ID: {payment.order_id}</p>
-                <p className="card-text">Amount: {payment.amount}</p>
-                <p className="card-text">Payment Date: {payment.payment_date.toString()}</p>
-            </div>
-        </div>
-    );
-};
-
-const viewPayment = (payments: Payment[]) => {
-    return (
-        <div className="container">
-            <div className="row">
-                {payments.map(payment => (
-                    <div className="col-lg-4 col-md-6 mb-4" key={payment.id}>
-                        <PaymentCard payment={payment}/>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-};
-*/
 
 export default viewPayment;
