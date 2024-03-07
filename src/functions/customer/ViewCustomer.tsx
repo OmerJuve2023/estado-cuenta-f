cec48d94ca481215a0901fc90cffb30a36a8575b
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-03-07 08:58:55 -0500

    update styles to tables

--- src/functions/customer/ViewCustomer.tsx
+++ src/functions/customer/ViewCustomer.tsx
@@ -13,10 +13,14 @@ const CustomerCard: React.FC<CustomerCardProps> = ({customer, onEdit, onDelete})
     return (
         <div className="card">
             <div className="card-body">
-                <p className="card-text"><b className={"text-black"}>{customer.name}</b></p>
-                <p className="card-text"><b>Email:</b><i className={"ms-2 text-black"}>{customer.email}</i></p>
-                <p className="card-text"><b>Teléfono</b><i className={"ms-2 text-black"}>{customer.phone}</i></p>
-                <p className="card-text"><b>Dirección</b><i className={"ms-2 text-black"}>{customer.address}</i></p>
+                <p className="card-text fw-bold text-center h5" style={{color: "blueviolet"}}>{customer.name}</p>
+                <p className="card-text fw-medium">Email:<span className={"ms-2"} style={{color: "blueviolet"}}>
+                    {customer.email}</span></p>
+                <p className="card-text fw-medium">Teléfono<span className={"ms-2"} style={{color: "blueviolet"}}>
+                    {customer.phone}</span></p>
+                <p className="card-text fw-medium">Dirección
+                    <i className={"ms-2"}style={{color: "blueviolet"}}>
+                        {customer.address}</i></p>
                 <div className="d-flex justify-content-end align-items-center mt-3">
                     <button className="btn transparent-btn me-2" onClick={() => onEdit(customer)}>
                         <FaEdit/> Editar
