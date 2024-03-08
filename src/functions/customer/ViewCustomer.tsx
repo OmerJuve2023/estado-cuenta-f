dd7ae417f0f8aa9ad408162c7e8354394d453744
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-03-07 22:50:18 -0500

    update view ViewCustomer.tsx

--- src/functions/customer/ViewCustomer.tsx
+++ src/functions/customer/ViewCustomer.tsx
@@ -14,13 +14,13 @@ const CustomerCard: React.FC<CustomerCardProps> = ({customer, onEdit, onDelete})
         <div className="card">
             <div className="card-body">
                 <p className="card-text fw-bold text-center h5" style={{color: "blueviolet"}}>{customer.name}</p>
-                <p className="card-text fw-medium">Email:<span className={"ms-2"} style={{color: "blueviolet"}}>
+                {/*<p className="card-text fw-medium">Email:<span className={"ms-2"} style={{color: "blueviolet"}}>
                     {customer.email}</span></p>
                 <p className="card-text fw-medium">Teléfono<span className={"ms-2"} style={{color: "blueviolet"}}>
                     {customer.phone}</span></p>
                 <p className="card-text fw-medium">Dirección
                     <i className={"ms-2"}style={{color: "blueviolet"}}>
-                        {customer.address}</i></p>
+                        {customer.address}</i></p>*/}
                 <div className="d-flex justify-content-end align-items-center mt-3">
                     <button className="btn transparent-btn me-2" onClick={() => onEdit(customer)}>
                         <FaEdit/> Editar
